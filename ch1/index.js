31fbea8c3e0f8c7a5dd3f2dcd5bc0907b9183036
Daniel Casper <dcasper@nectarom.com>
2016-01-23 14:02:46 -0600

    Reconfig to basic project.

---
+++ ch1/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+// Creating a Hapi server
+
+const server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+// Setting up routes
+
+server.route([{
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+
+        reply('Hello World!');
+    }
+}, {
+    method: 'GET',
+    path: '/json',
+    handler: function (request, reply) {
+
+        reply({ hello: 'World' });
+    }
+}]);
+
+// Registering the Good plugin
+
+server.register({
+    register: require('good'),
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: { response: '*' }
+        }]
+    }
+}, (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    // Starting the server
+
+    server.start(() => {
+
+        console.log('Server running at:', server.info.uri);
+    });
+});
