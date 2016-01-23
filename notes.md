d51126687819172426f27a85f3a77eb2db5a9545
Daniel Casper <dcasper@nectarom.com>
2016-01-22 20:59:19 -0600

    Use confidence to generate runtime configs.

--- notes.md
+++ notes.md
@@ -10,8 +10,20 @@ One function can only apply plugins to one connection at a time.
 
 Hooray for async.auto. Hapi uses a lightweight version of Async called items. https://github.com/hapijs/items
 
+## Glue
 
+Glue lets you compose server settings. Keep the configs out of the server (duhhh declarative), and applies to the correct labels / connections. Super nice.
 
+## Rejoice
 
+Want to do it all from JSON config instead? Yes plz. Rejoice! Cli tool for this.
 
+## Confidence
 
+What if you want different configs per env? Could filter JSON. Blech.
+
+Could have multiple files. Multiple blech.
+
+Keep it single source of truth with Confidence. Extensible JSON with key/val merge.
+
+Confidence will generate the rejoice.json by applying the proper filter, merging $base with `confidence -c --filter.env="myenv" > rejoice.json`
--- src/server.js
+++ src/server.js
@@ -1,99 +1,131 @@
 'use strict';
 
-const Async = require('async');
-const Bell = require('bell');
-const Blipp = require('blipp');
 const Hapi = require('hapi');
-const HapiAuthCooke = require('hapi-auth-cookie');
-const Hoek = require('hoek');
-const Api = require('./api');
-const Authentication = require('./authentication');
-const Controllers = require('./controllers');
-const Models = require('./models');
-const Routes = require('./routes');
+const Glue = require('glue');
+
+// L'internals
 
 const internals = {
-    servers: {
-        http: {
+    manifest: {
+        connections: [{
             port: 8088,
-            host: '0.0.0.0',
             labels: ['http']
-        },
-        api: {
+        }, {
             port: 8088,
-            host: '0.0.0.0',
             labels: ['api']
-        }
-    },
-    options: {
-        files: {
-            relativeTo: __dirname
+        }],
+        plugins: {
+            bell: [{ 'select': 'http' }],
+            blipp: [{}],
+            'hapi-auth-cookie': [{ 'select': 'http' }],
+
+            './authentication': [{ 'select': ['http', 'api'] }],
+
+            './controllers': [{ 'select': ['http', 'api'] }],
+
+            './models': [{ 'select': ['http', 'api'] }],
+
+            './routes': [{ 'select': ['http'] }],
+
+            './api': [{ 'select': ['api'] }],
+
+            good: {
+                opsInterval: 5000,
+                reporters: [{
+                    'reporter': 'good-console',
+                    'events': { 'ops': '*', 'log': '*' }
+                }]
+            }
         }
     }
 };
 
-exports.init = function (callback) {
-
-    const server = new Hapi.Server();
-    server.connection(internals.servers.http);
-    server.connection(internals.servers.api);
-
-    server.path(internals.options.files.relativeTo);
-
-    server.on('request-error', (request, response) => {
-
-        console.log('request-error:');
-        console.dir(response);
-    });
-
-    const registerHttpPlugins = function (next) {
-
-        server.register([
-            Bell,
-            Blipp,
-            HapiAuthCooke,
-            Authentication,
-            Controllers,
-            Models,
-            Routes
-        ],
-        { select: 'http' },
-        (err) => {
-
-            return next(err);
-        });
-    };
-
-    const registerApiPlugins = function (next) {
-
-        server.register([
-            Blipp,
-            Controllers,
-            Models,
-            Api
-        ],
-        { select: 'api' },
-        (err) => {
-
-            return next(err);
-        });
-    };
-
-    Async.auto({
-        http: registerHttpPlugins,
-        api: registerApiPlugins
-    }, (err, data) => {
+Glue.compose(internals.manifest,
+    { relativeTo: __dirname }, (err, server) => {
 
         if (err) {
             console.log(`server.register err: ${err}`);
-            return callback(err);
         }
 
         server.start(() => {
 
-            return callback(null, server);
+            console.log('hi, i\'m a hapi server!');
         });
     });
 
-    exports.init(Hoek.ignore);
-};
+// const Async = require('async');
+// const Bell = require('bell');
+// const Blipp = require('blipp');
+// const HapiAuthCooke = require('hapi-auth-cookie');
+// const Hoek = require('hoek');
+// const Api = require('./api');
+// const Authentication = require('./authentication');
+// const Controllers = require('./controllers');
+// const Models = require('./models');
+// const Routes = require('./routes');
+
+// exports.init = function (callback) {
+
+//     const server = new Hapi.Server();
+//     server.connection(internals.servers.http);
+//     server.connection(internals.servers.api);
+
+//     server.path(internals.options.files.relativeTo);
+
+//     server.on('request-error', (request, response) => {
+
+//         console.log('request-error:');
+//         console.dir(response);
+//     });
+
+//     const registerHttpPlugins = function (next) {
+
+//         server.register([
+//             Bell,
+//             Blipp,
+//             HapiAuthCooke,
+//             Authentication,
+//             Controllers,
+//             Models,
+//             Routes
+//         ],
+//         { select: 'http' },
+//         (err) => {
+
+//             return next(err);
+//         });
+//     };
+
+//     const registerApiPlugins = function (next) {
+
+//         server.register([
+//             Blipp,
+//             Controllers,
+//             Models,
+//             Api
+//         ],
+//         { select: 'api' },
+//         (err) => {
+
+//             return next(err);
+//         });
+//     };
+
+//     Async.auto({
+//         http: registerHttpPlugins,
+//         api: registerApiPlugins
+//     }, (err, data) => {
+
+//         if (err) {
+//             console.log(`server.register err: ${err}`);
+//             return callback(err);
+//         }
+
+//         server.start(() => {
+
+//             return callback(null, server);
+//         });
+//     });
+
+//     exports.init(Hoek.ignore);
+// };
