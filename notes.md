f12f599a41651765609e2f3d26c32e52f34efb85
Daniel Casper <dcasper@nectarom.com>
2016-01-22 20:27:44 -0600

    Create basic plugin registration method.

---
+++ notes.md
@@ -0,0 +1,17 @@
+# Server
+
+Typical SPA is to server FE Framework. Can be isomorphic, or separate. Consider separate.
+
+Use case for labels. Mount plugins relative to labels.
+
+Server methods are also available on anything passed to plugin (first arugment passed to plugin register object, duhhh).
+
+One function can only apply plugins to one connection at a time.
+
+Hooray for async.auto. Hapi uses a lightweight version of Async called items. https://github.com/hapijs/items
+
+
+
+
+
+
--- src/server.js
+++ src/server.js
@@ -1,27 +1,99 @@
 'use strict';
 
+const Async = require('async');
+const Bell = require('bell');
+const Blipp = require('blipp');
 const Hapi = require('hapi');
+const HapiAuthCooke = require('hapi-auth-cookie');
+const Hoek = require('hoek');
+const Api = require('./api');
+const Authentication = require('./authentication');
+const Controllers = require('./controllers');
+const Models = require('./models');
+const Routes = require('./routes');
 
-const server = new Hapi.Server();
-
-server.connection({
-    host: 'localhost',
-    port: 8088
-});
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-
-        return reply('hi daniel');
+const internals = {
+    servers: {
+        http: {
+            port: 8088,
+            host: '0.0.0.0',
+            labels: ['http']
+        },
+        api: {
+            port: 8088,
+            host: '0.0.0.0',
+            labels: ['api']
+        }
+    },
+    options: {
+        files: {
+            relativeTo: __dirname
+        }
     }
-});
+};
 
-server.start((err) => {
+exports.init = function (callback) {
 
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});
+    const server = new Hapi.Server();
+    server.connection(internals.servers.http);
+    server.connection(internals.servers.api);
+
+    server.path(internals.options.files.relativeTo);
+
+    server.on('request-error', (request, response) => {
+
+        console.log('request-error:');
+        console.dir(response);
+    });
+
+    const registerHttpPlugins = function (next) {
+
+        server.register([
+            Bell,
+            Blipp,
+            HapiAuthCooke,
+            Authentication,
+            Controllers,
+            Models,
+            Routes
+        ],
+        { select: 'http' },
+        (err) => {
+
+            return next(err);
+        });
+    };
+
+    const registerApiPlugins = function (next) {
+
+        server.register([
+            Blipp,
+            Controllers,
+            Models,
+            Api
+        ],
+        { select: 'api' },
+        (err) => {
+
+            return next(err);
+        });
+    };
+
+    Async.auto({
+        http: registerHttpPlugins,
+        api: registerApiPlugins
+    }, (err, data) => {
+
+        if (err) {
+            console.log(`server.register err: ${err}`);
+            return callback(err);
+        }
+
+        server.start(() => {
+
+            return callback(null, server);
+        });
+    });
+
+    exports.init(Hoek.ignore);
+};
